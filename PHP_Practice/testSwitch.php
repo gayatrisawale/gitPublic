8e134509d9be9d8112b9ec8c0c107f5801a8cfb8
Gayatri <gayatrisawale.gs@gmail.com>
2026-01-15 01:08:56 +0530

    TestSwitch

--- PHP_Practice/testSwitch.php
+++ PHP_Practice/testSwitch.php
@@ -1,41 +1,76 @@
 <?php
-$ch='b';
+
+$ch='a';
+$test=['Another', 'Variable' ,'to', 'Test'];
 
 Switch($ch){
-
     case 'a':
-        echo"in a";
-        break;
-    
+       callA($ch, $test);
+       break; 
     case 'b':
-        echo"call fun";
-        callB();
+        echo 'b is selected';
         break;
-
-    default:
-        echo"in default"; 
-        break;
-}
-function callB(){
-    echo "we are in B function";
+    case 'c':
+        echo 'b is selected'; 
+        break;   
+    default: 
+        echo 'Test Completed'; 
+        break;   
 }
 
-echo"</br>";
+function callA($ch, $test){
+    echo $ch.' is selected. <pre>'; print_r($test);
+}
 
+echo"</br>---------Recursive Factorial---------";
 
-  $list=[3,5,2,4,1];
-  $arrCount=count($list);
-  for( $i=0; $i<$arrCount; $i++){ 
-  	for( $j=$i+1; $j<$arrCount; $j++){ 
-    	if($list[$i]>$list[$j]){
-        	$Temp=$list[$j];
-        	$list[$j]=$list[$i];
-            $list[$i]=$Temp;
+function Fact($num){
+    if($num==1)
+    { return 1;}
+    else{
 
-          //echo"<pre>"; print_r($list);
-        }  
+        $f =$num*Fact($num-1);
+        return $f;
     }
-  }
-  echo"<pre>"; print_r($list);
+}
 
+echo "</br>Fact Value: ".Fact(5);
+
+echo"</br>---------Tower of henoi ---------";
+
+function towerOfHanoi($n, $source, $auxiliary, $target) {
+    if ($n == 1) {
+        echo "<br/>Move disk 1 from $source $auxiliary $target<br>";
+        return;
+    }
+
+    towerOfHanoi($n - 1, $source, $target, $auxiliary);
+
+    echo "<br/>Move disk $n from $source $auxiliary $target<br>";
+
+    towerOfHanoi($n - 1, $auxiliary, $source, $target);
+}
+
+// Run with 3 disks
+echo "<br><strong>Result:</strong><br><br>";
+towerOfHanoi(3, 'S', 'A', 'T'); 
+
+echo"</br>---------Impolde and Explode ---------";
+$str="Test the explode function";
+$lsArray=explode(' ', $str);
+$orgStr=implode(',', $lsArray);
+
+print_r($lsArray);
+print_r($orgStr);
+
+echo"</br>---------Reverse String without inBuilt Str Function---------";
+$strName= 'Notebook';
+//echo"</br> Reveresed Strig InBuilt: ".strrev($strName);
+
+$stLen=strlen($strName);
+for( $i=$stLen-1; $i>=0; $i--){
+    $strRev[]=($strName[$i]);
+}
+echo "</br>Reverse String=". implode('',$strRev)
 ?>
+
